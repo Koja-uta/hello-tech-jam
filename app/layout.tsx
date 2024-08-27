345064a9ebc2d6e5f55aa4754cb0742b43ab5572
KojaUta <mi201309@edu.okinawa-ct.ac.jp>
2024-08-27 02:01:44 +0000

    debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
  
-const inter = Inter({ subsets: ["latin"] });
+
  
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body >
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
